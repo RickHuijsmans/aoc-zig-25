5c310aeeaf9f99ffee567d5559c7923a1a66054a
Rick Huijsmans <26407053+RickHuijsmans@users.noreply.github.com>
2025-12-01 15:55:26 +0100

    wip

--- src/day1.zig
+++ src/day1.zig
@@ -57,6 +57,47 @@ const Day1Impl = struct {
 
         return seenZeros;
     }
+
+    pub fn solveSample2(self: *const Day1Impl) !u64 {
+        return self.solve2(&sample1);
+    }
+
+    pub fn solve2(_: *const Day1Impl, input: *const String) !u64 {
+        var rows = try linq.split(input, "\n", true);
+        const totalDigits: i16 = 100;
+        const startPos: i16 = 50;
+        var currentPos = startPos;
+        var seenZeros: u64 = 0;
+
+        std.debug.print("\nStart position: {}\n", .{startPos});
+
+        while (rows.next()) |row| {
+            const clockwise = row[0] == 'R';
+            const amount = try String.parseInt(row[1..], i16);
+            const startZeros = seenZeros;
+            const prevPos = currentPos;
+
+            std.debug.print("Move {s} {} times", .{ row[0..1], amount });
+
+            for (0..@intCast(amount)) |_| {
+                currentPos += if (clockwise) 1 else -1;
+                currentPos = @mod(currentPos, totalDigits);
+
+                if (currentPos == 0) {
+                    seenZeros += 1;
+                }
+            }
+
+            std.debug.print("\n", .{});
+            if (seenZeros != startZeros) {
+                std.debug.print("Position: {} -> {} with {} zeroes\n\n", .{ prevPos, currentPos, seenZeros - startZeros });
+            } else {
+                std.debug.print("Position: {} => {}\n\n", .{ prevPos, currentPos });
+            }
+        }
+
+        return seenZeros;
+    }
 };
 
 test "Sample 1" {
@@ -77,4 +118,24 @@ test "Solve 1" {
     const input = try file.getInput(alloc, "./src/day1.input");
     const result = try context.solve1(&input);
     try std.testing.expectEqual(982, result);
+}
+
+test "Sample 2" {
+    const pageAlloc = std.heap.page_allocator;
+    var arena = std.heap.ArenaAllocator.init(pageAlloc);
+    const alloc = arena.allocator();
+    const context = Day1Impl{ .allocator = alloc };
+    const result = try context.solveSample2();
+    try std.testing.expectEqual(6, result);
+}
+
+test "Solve 2" {
+    const pageAlloc = std.heap.page_allocator;
+    var arena = std.heap.ArenaAllocator.init(pageAlloc);
+    const alloc = arena.allocator();
+    const context = Day1Impl{ .allocator = alloc };
+
+    const input = try file.getInput(alloc, "./src/day1.input");
+    const result = try context.solve2(&input);
+    try std.testing.expectEqual(6106, result);
 }
